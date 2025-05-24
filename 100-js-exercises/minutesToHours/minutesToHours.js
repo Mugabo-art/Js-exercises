85eeaa8a8ba6e171508e9c1192a3aae80afeac08
Mugabo-art <m.kbonheur12@gmail.com>
2025-05-24 17:35:04 +0200

    Update minutesToHours.js

--- 100-js-exercises/minutesToHours/minutesToHours.js
+++ 100-js-exercises/minutesToHours/minutesToHours.js
@@ -1,3 +1,4 @@
+/*Write a function named minutesToHours that receives a number of minutes as parameter and returns a number representing the same amount of time but in hours.*/
 function minutesToHours(minutes) {
     return minutes / 60
 }
